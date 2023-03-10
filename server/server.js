aa890cb40f9f8b5c6a2a8dcffebe8fa8168a9023
Matthew1906 <matthew.mulyono001@binus.ac.id>
2023-03-10 17:06:09 +0800

    Setup firebase basic auth on server

---
+++ server/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+import cors from "cors";
+import config from "./config/index.js";
+import db from "./config/db.js";
+import userRouter from "./routes/user.js";
+
+const app = express();
+
+db(config.MONGO_URI, app);
+
+app.use(cors({ origin: true }));
+app.use(express.json());
+app.use("/api/user", userRouter);
+
+app.listen(config.PORT, () =>
+  console.log(`App listening on PORT ${config.PORT}`)
+);
+
+app.get("/", async(req, res)=>{
+    res.json({message:"Hello, It's me! I've been wondering if after all these years you would like to meet"})
+})
