9df581d41ee6be43d2a884ccf11c72adad00e0e3
Matthew1906 <matthew.mulyono001@binus.ac.id>
2023-03-25 20:57:33 +0800

    Fix syntax error in footer

--- client/src/components/partials/index.js
+++ client/src/components/partials/index.js
@@ -1,5 +1,5 @@
 import DesktopNavigation from "./DesktopNavigation";
 import MobileNavigation from "./MobileNavigation";
-import Footer from "./Footer";
+import Footer from "./footer";
 
 export { DesktopNavigation, Footer, MobileNavigation };
