b85370b7f136079a7fa04c7e5d2bb60032b4209c
Matthew1906 <matthew.mulyono001@binus.ac.id>
2023-04-06 19:11:16 +0800

    Only use google OAuth (for now)

--- client/src/components/icons/AuthIcons.jsx
+++ client/src/components/icons/AuthIcons.jsx
@@ -24,29 +24,29 @@ const AuthIcons = ({purpose})=>{
                 }).catch((err)=>console.log(err.message));
         }
     }
-    const facebookHandler = ()=>{
-        if(purpose === 'login'){
-            loginWithFacebook()
-                .then((res)=>{
-                    const user = res.user;
-                    registerOAuth({ username: user.displayName, email:user.email })
-                        .then(()=>navigate("/"))
-                        .catch((err)=>console.log(err))
-                }).catch((err)=>console.log(err.message));
-        } else if(purpose ==='register'){
-            loginWithFacebook()
-                .then((res)=>{
-                    const user = res.user;
-                    registerOAuth({ username: user.displayName, email:user.email })
-                        .then(()=>logout().then(()=>navigate("/")))
-                        .catch((err)=>console.log(err))
-                }).catch((err)=>console.log(err.message));
-        }
-    }
+    // const facebookHandler = ()=>{
+    //     if(purpose === 'login'){
+    //         loginWithFacebook()
+    //             .then((res)=>{
+    //                 const user = res.user;
+    //                 registerOAuth({ username: user.displayName, email:user.email })
+    //                     .then(()=>navigate("/"))
+    //                     .catch((err)=>console.log(err))
+    //             }).catch((err)=>console.log(err.message));
+    //     } else if(purpose ==='register'){
+    //         loginWithFacebook()
+    //             .then((res)=>{
+    //                 const user = res.user;
+    //                 registerOAuth({ username: user.displayName, email:user.email })
+    //                     .then(()=>logout().then(()=>navigate("/")))
+    //                     .catch((err)=>console.log(err))
+    //             }).catch((err)=>console.log(err.message));
+    //     }
+    // }
     return <div className="flex justify-center gap-5">
         <Button theme='red' type='button' onClick={googleHandler}><BsGoogle /></Button>
-        <Button theme='blue' type='button' onClick={facebookHandler}><BsFacebook /></Button>
-        <Button theme='yellow' type='button' ><BsMicrosoft /></Button>
+        {/* <Button theme='blue' type='button' onClick={facebookHandler}><BsFacebook /></Button> */}
+        {/* <Button theme='yellow' type='button' ><BsMicrosoft /></Button> */}
     </div>
 }
 
