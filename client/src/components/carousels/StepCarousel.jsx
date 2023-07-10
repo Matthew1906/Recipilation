7a759d5491fc69f616c6d1bc3413f54b92d8242e
Matthew1906 <matthew.mulyono001@binus.ac.id>
2023-07-10 14:29:23 +0700

    Debug Step Carousel

--- client/src/components/carousels/StepCarousel.jsx
+++ client/src/components/carousels/StepCarousel.jsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { Swiper, SwiperSlide } from 'swiper/react';
 import "swiper/css";
 import "swiper/css/effect-coverflow";
@@ -7,15 +7,13 @@ import "swiper/css/pagination";
 import { EffectCoverflow, Navigation, Pagination} from 'swiper';
 import "./styles.css";
 
-/* TO DEBUG:
-- In the initial render, the order of the slide images starts from slide 3, while the current index is 0
-- setting the initialSlide did not work at all
-- However when the user starts navigating the slides, it goes back to normal
-- When inspecting the elements, it seems that the order of the slides starts from 3 to the end, and then back to 1
-
-*/
 const StepCarousel = ({items})=>{
-    const [current, setCurrent] = useState(0);
+    const [ current, setCurrent ] = useState(0);
+    const [ elements, setElements ] = useState([]); 
+    useEffect(()=>{
+      const els = items.sort((a,b)=>a.index-b.index);
+      setElements(els);    
+    }, [items]);
     return(
         <div className='p-5'>
           <Swiper
@@ -37,17 +35,22 @@ const StepCarousel = ({items})=>{
             modules={[EffectCoverflow, Pagination, Navigation]}
             className="step w-full text-black"
             onSlideChange={(swiper)=>setCurrent(swiper.realIndex)}
-            initialSlide={-1}
+            onAfterInit={(swiper)=>{
+              swiper.slideNext();
+              swiper.slideNext();
+              swiper.slideNext();
+              setCurrent(0);
+            }}
           >
-            {items.sort((a,b)=>a.index-b.index).map((item)=>(
+            {elements.map((item)=>(
                 <SwiperSlide key={item?.index}>
                     <img src={item?.image??'/images/not-exist.jpg'}  alt={item?.title} className='w-50 aspect-[3/2]'/>
                 </SwiperSlide>
             ))}
         </Swiper>
         <div className='my-5 text-center text-nunito'>
-          <strong className='text-xl md:text-3xl font-semibold'>{items[current]?.index}. {items[current]?.title}</strong>
-          <p className="mt-3 px-24 text-lg md:text-2xl font-light text-justify">{items[current]?.details}</p>
+          <strong className='text-xl md:text-3xl font-semibold'>{elements[current]?.index}. {elements[current]?.title}</strong>
+          <p className="mt-3 px-24 text-lg md:text-2xl font-light text-justify">{elements[current]?.details}</p>
         </div>
       </div>
     )
