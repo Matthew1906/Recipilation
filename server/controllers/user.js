a64dcc4fabfee02cb92e4827a8583aa2bb46bc97
Matthew1906 <matthew.mulyono001@binus.ac.id>
2023-10-18 12:44:19 +0700

    Auth bug fix

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -8,23 +8,23 @@ export const authenticateUser = async(req, res)=>{
     const { email, username, password=null, type } = req.body;
     try{
         if(type=='oauth'){
-        const firebaseUser = await firebaseAdmin.auth.getUserByEmail(email);
-        const newUser = new User({username, email, firebaseId:firebaseUser.uid, slug:slugify(username)});
-        const checkUser = await User.find({email});
-        if(checkUser.length == 0){
-            newUser.save();
-        }
+            const firebaseUser = await firebaseAdmin.auth.getUserByEmail(email);
+            const newUser = new User({username, email, firebaseId:firebaseUser.uid, slug:slugify(username.toLowerCase())});
+            const checkUser = await User.find({email});
+            if(checkUser.length == 0){
+                newUser.save();
+            }
             return res.status(200).json({ success: "Account created successfully. Please sign in." });
         }
         else{
-        if (!email || !username || !password) {
-            return res.status(400).json({ error:"Invalid request body. Must contain email, password, and username." });
-        }
-        const newFirebaseUser = await firebaseAdmin.auth.createUser({ email, password });
-        if (newFirebaseUser) {
-            const newUser = new User({username, email, firebaseId:newFirebaseUser.uid, slug:slugify(username)});
-            newUser.save();
-        }
+            if (!email || !username || !password) {
+                return res.status(400).json({ error:"Invalid request body. Must contain email, password, and username." });
+            }
+            const newFirebaseUser = await firebaseAdmin.auth.createUser({ email, password });
+            if (newFirebaseUser) {
+                const newUser = new User({username, email, firebaseId:newFirebaseUser.uid, slug:slugify(username.toLowerCase())});
+                newUser.save();
+            }
             return res.status(200).json({ success: "Account created successfully. Please sign in." });
         }
     } catch (err) {
